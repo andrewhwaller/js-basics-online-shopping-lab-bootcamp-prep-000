6e961a8839219d21fc9b6e0b564fd0347ddd371f
Andrew Waller <andrewhwaller@gmail.com>
2019-03-12 01:22:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function viewCart() {
   if (cart.length === 0) {
     return "Your shopping cart is empty."
     } else {
-      for ([i] in cart) {
+      for (item in cart) {
         return `In your cart, you have ${itemName} at ${$price}.`
         }
       }
