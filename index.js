c52003d375f394104ba9e8f1a187675e872f92cf
Andrew Waller <andrewhwaller@gmail.com>
2019-03-12 03:09:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (var i = 0; i < cart.length; i++ ) {
+  for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
       cart.splice([i].itemName)
     }
