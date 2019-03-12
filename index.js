2e26d1e0a382fecaf8ce42aaeebe7198ea73c80e
Andrew Waller <andrewhwaller@gmail.com>
2019-03-12 01:33:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,23 +10,20 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  return generateCartItem()
-  getCart().push(item);
-  return `${item.itemName} has been added to your cart.`
-}
-
-function generateCartItem(itemName) {
   var price = Math.floor(Math.random() * 101)
   var item = new Object({
     itemName: item,
     itemPrice: price
   });
+  getCart().push(item);
+  return `${item.itemName} has been added to your cart.`
 }
 
 function viewCart() {
   if (cart.length === 0) {
     return "Your shopping cart is empty."
     } else {
+      var cartDescription = 'In your cart, you have '
       for (item in cart) {
         return `In your cart, you have ${itemName} at ${$price}.`
         }
